968dfc6fdb59d2ad792856b9574fab567feb44b0
Roxana D.P <Roxana D.P@DESKTOP-N4IQQTI>
2021-09-14 22:40:19 -0600

    Creando el Programa Frase Motivadora

---
+++ Frase Motivadora/Frase Motivadora/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frase_Motivadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("En este lugar no miramos mucho hacia atrás.Camina hacia el futuro, abriendo nuevas puertas y probando cosas nuevas, se curioso... porque nuestra curiosidad siempre nos conduce por nuevos caminos.");
+        }
+    }
+}
