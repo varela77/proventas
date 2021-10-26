e24ac88eb12fa59182846fca9bb442ded8dacbca
varela77 <90734107+varela77@users.noreply.github.com>
2021-10-25 21:40:19 -0700

    formularios y agregados

---
+++ 2/rentas/Win.rentas/FormMenu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.rentas
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void loginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formLogin = new Formlogin();
+            formLogin.ShowDialog();
+        }
+
+        private void productosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formproductos = new formproductos();
+            formproductos.MdiParent = this;
+            formproductos.Show();
+        }
+
+        private void clientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formRentas = new FormRentas();
+            formRentas.MdiParent = this;
+            formRentas.Show();
+        }
+
+        private void rentarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formRentas = new FormRentas();
+            formRentas.MdiParent = this;
+            formRentas.Show();
+        }
+
+        private void reporteProductoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formRepoDi = new FormRepoDi();
+            formRepoDi.MdiParent = this;
+            formRepoDi.Show();
+        }
+
+        private void reporteClientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formRepoProd = new FormRepoProd();
+            formRepoProd.MdiParent = this;
+            formRepoProd.Show();
+        }
+    }
+}
